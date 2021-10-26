c5e7b954b3bb76dc0357469772ea0346d2cee9bd
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-26 13:57:07 -0500

    woo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>                
+      <h1 className="my-class">Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>                
+      <ul>
+        <li>Learn React</li>
+        <li>Climb MT. Everest</li>
+        <li>Run a marathon</li>
+        <li>Reed the dogs</li>
+      </ul>           
+    </>    
   );
+
 }
 
 export default App;
